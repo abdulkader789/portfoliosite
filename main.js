13a03b661e5e09d3de81751dfcb10bfa18294994
Md. Abdul Kader <abdulemail789@gmail.com>
2023-09-04 23:06:03 +0600

    added carousel from tailwind

--- main.js
+++ main.js
@@ -1,21 +1,8 @@
-const slidesContainer = document.querySelector(".slides-container");
-const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
-const prevButton = document.querySelector(".prev");
-const nextButton = document.querySelector(".next");
-
-nextButton.addEventListener("click", () => {
-  slidesContainer.scrollLeft += slideWidth;
-});
-
-prevButton.addEventListener("click", () => {
-  slidesContainer.scrollLeft -= slideWidth;
-});
-
 const nav_about = document.getElementById("nav-about");
 const nav_project = document.getElementById("nav-project");
 const about_section = document.getElementById("about-section");
 const project_section = document.getElementById("project-section");
-about_section.style.display = "none";
+about_section.style.display = "flex";
 project_section.style.display = "none";
 
 nav_about.addEventListener("click", () => {
